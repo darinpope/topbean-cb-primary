d8a93b0052eab6b1ebf7c9ef2e4c4f317edab653
Darin Pope <darin@planetpope.com>
2020-01-23 13:54:02 -0500

    project-b update

---
+++ project-b/eb-package.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+rm -rf eb-package-prep
+rm -f project-b-1.0-SNAPSHOT.zip
+
+mkdir -p eb-package-prep/bin
+mkdir -p eb-package-prep/conf
+mkdir -p eb-package-prep/lib
+
+cp Dockerfile eb-package-prep/Dockerfile
+cp Dockerrun.aws.json eb-package-prep/Dockerrun.aws.json
+cp -r target/universal/stage/bin/* eb-package-prep/bin/
+cp -r target/universal/stage/conf/* eb-package-prep/conf/
+cp -r target/universal/stage/lib/* eb-package-prep/lib/
+
+cd eb-package-prep || exit
+zip -r ../project-b-1.0-SNAPSHOT.zip ./*
